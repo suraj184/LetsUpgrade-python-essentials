5201cc721595bc052fd34722035e54c481165710
suraj184 <68589950+suraj184@users.noreply.github.com>
2020-07-28 20:46:54 +0530

    Update dictionary.py

--- Day 6/dictionary.py
+++ Day 6/dictionary.py
@@ -1,5 +1,4 @@
-List1=[1,2,3,4,5,6,7]
-List2=['a','b','c','d','e']
-List3=[]    
-[List3.append((List1[each],List2[each])) for each in range(min(len(List1),len(List2)))]
-print(dict(List3))
+list1 = [1,2,3,4,5,7]
+list2 = ['a','b','c','d','e']
+dict1= {list1[each]:list2[each] for each in range(min(len(list1),len(list2)))}
+print(d1)
